0b663352bda99a803706d4d988686bba54307ca4
Mondeep Gogoi <mondeepgogoi@Mondeeps-MacBook-Air.local>
2024-08-03 13:00:15 +0530

    basic fixes

--- App.tsx
+++ App.tsx
@@ -2,14 +2,35 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ChatbotScreen from './screens/ChatbotScreen';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Chatbot" component={ChatbotScreen} />
+      <Tab.Navigator  
+        screenOptions={{
+          headerShown: false,
+          tabBarIcon: () => null,
+          tabBarActiveTintColor:'blue',
+          tabBarInactiveTintColor:'grey',
+          tabBarLabelStyle: {
+            fontSize: 16,
+            fontWeight: 'bold',
+            textAlign: 'center',
+          },
+          tabBarStyle: {
+            paddingBottom: 10,
+            height: 60,
+          },
+      }}
+      >
+        <Tab.Screen  name="Chatbot" component={ChatbotScreen}  options={{
+          tabBarLabel: ({ color }) => (
+            <Text style={{ textAlign: 'center', color }}>{'Chatbot'}</Text>
+          ),
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- screens/ChatbotScreen.tsx
+++ screens/ChatbotScreen.tsx
@@ -1,125 +1,157 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, Text, ScrollView, Alert } from 'react-native';
+import { View, TextInput, Text, Alert, TouchableOpacity, FlatList, ListRenderItem, Image, StyleSheet } from 'react-native';
 import Voice from '@react-native-voice/voice';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ChatbotScreen = () => {
-  const [query, setQuery] = useState('');
-  const [recipes, setRecipes] = useState([]);
-  const [error, setError] = useState('');
-  const [isListening, setIsListening] = useState(false);
-
-  useEffect(() => {
-    Voice.onSpeechResults = onVoiceResults;
-    Voice.onSpeechError = onVoiceError;
-    Voice.onSpeechStart = onVoiceStart;
-    Voice.onSpeechEnd = onVoiceEnd;
-    return () => {
-      Voice.destroy().then(Voice.removeAllListeners);
-    };
-  }, []);
-
-  const fetchRecipes = async (query: string) => {
-    console.log('whats going?',query)
-    try {
-      const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch`, {
-        params: {
-          query,
-          apiKey: '292bd43fd3e64acfb08e9cc44290c1dc',
-        },
-      });
-      setRecipes(response.data.results);
-      console.log('results',response.data.results)
-      await AsyncStorage.setItem('searchedRecipes', JSON.stringify(response.data.results));
-    } catch (error) {
-      setError('Failed to fetch recipes. Please try again.');
-    }
-  };
-
-  const onVoiceStart = () => {
-    console.log('listen')
-    setIsListening(true);
-  };
-
-  const onVoiceEnd = () => {
-    console.log('stop')
-    setIsListening(false);
-  };
-
-  const onVoiceResults = (e: any) => {
-    const text = e.value;
-    console.log('speak',text)
-    setQuery(text);
-    fetchRecipes(text);
-  };
-
-  const onVoiceError = (e: any) => {
-    setError('Voice recognition failed. Please try again.');
-    setIsListening(false);
-  };
-
-  const startListening = async () => {
-    console.log('listen to my song')
-    setError('');
-    setRecipes([]);
-    setQuery('');
-    try {
-      await Voice.start('en-US');
-      setIsListening(true);
-    } catch (e) {
-      console.error(e);
-      setIsListening(false);
-    }
-  };
-
-  const stopListening = async () => {
-    console.log('stopp to my song')
-    try {
-      await Voice.stop();
-      await Voice.destroy();
-      setIsListening(false);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const saveToShoppingList = async (recipe: any) => {
-    try {
-      const storedRecipes = await AsyncStorage.getItem('shoppingList');
-      const shoppingList = storedRecipes ? JSON.parse(storedRecipes) : [];
-      const updatedList = [...shoppingList, recipe];
-      await AsyncStorage.setItem('shoppingList', JSON.stringify(updatedList));
-      console.log('show items saved',updatedList)
-      Alert.alert('Success', 'Recipe added to your shopping list.');
-    } catch (error) {
-      Alert.alert('Error', 'Failed to save recipe. Please try again.');
-    }
-  };
-
-  return (
-    <View style={{backgroundColor:'red'}}>
-      <TextInput
-        value={query}
-        onChangeText={setQuery}
-        placeholder="Type your recipe query"
-      />
-      <Button title="Search" onPress={() => fetchRecipes(query)} />
-      <Button title={isListening ? "Stop Voice Search" : "Start Voice Search"} onPress={isListening ? stopListening : startListening} />
-
-      {error ? <Text>{error}</Text> : null}
-
-      <ScrollView>
-        {recipes.map((recipe: any) => (
-          <View key={recipe.id}>
-            <Text>{recipe.title}</Text>
-            <Button title="Save Ingredients" onPress={() => saveToShoppingList(recipe)} />
-            <Text>{recipe.instructions}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
-  );
+// Defining the type for receipes
+type Recipe = {
+    id: number;
+    title: string;
+    image: string;
 };
 
+const ChatbotScreen = () => {
+    const [query, setQuery] = useState<string>('');
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [error, setError] = useState<string>('');
+    const [isListening, setIsListening] = useState<boolean>(false);
+
+    useEffect(() => {
+        Voice.onSpeechResults = onVoiceResults;
+        Voice.onSpeechError = onVoiceError;
+        Voice.onSpeechStart = onVoiceStart;
+        Voice.onSpeechEnd = onVoiceEnd;
+        return () => {
+            Voice.destroy().then(Voice.removeAllListeners);
+        };
+    }, []);
+
+    const fetchRecipes = async (query: string) => {
+        console.log('whats going?', query);
+        try {
+            const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch', {
+                params: {
+                    query,
+                    apiKey: '292bd43fd3e64acfb08e9cc44290c1dc',
+                },
+            });
+            setRecipes(response.data.results);
+            await AsyncStorage.setItem('searchedRecipes', JSON.stringify(response.data.results));
+        } catch (error) {
+            setError('Sorry,failed to fetch recipes. Please try again.');
+        }
+    };
+
+    const onVoiceStart = () => {
+        setIsListening(true);
+    };
+
+    const onVoiceEnd = () => {
+        setIsListening(false);
+    };
+
+    const onVoiceResults = (e: any) => {
+        const text = e.value;
+        setQuery(text);
+        fetchRecipes(text);
+    };
+
+    const onVoiceError = () => {
+        setError('Voice recognition failed. Please try again.');
+        setIsListening(false);
+    };
+
+    const startListening = async () => {
+        setError('');
+        setRecipes([]);
+        setQuery('');
+        try {
+            await Voice.start('en-US');
+            setIsListening(true);
+        } catch (e) {
+            console.error(e);
+            setIsListening(false);
+        }
+    };
+
+    const stopListening = async () => {
+        try {
+            await Voice.stop();
+            await Voice.destroy();
+            setIsListening(false);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const saveToShoppingList = async (recipe: Recipe) => {
+        try {
+            const storedRecipes = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = storedRecipes ? JSON.parse(storedRecipes) : [];
+            const updatedList = [...shoppingList, recipe];
+            await AsyncStorage.setItem('shoppingList', JSON.stringify(updatedList));
+            Alert.alert('Success', 'Recipe added to your shopping list.');
+        } catch (error) {
+            Alert.alert('Error', 'Sorry,failed to save recipe. Please try again.');
+        }
+    };
+
+    const renderRecipiesItem: ListRenderItem<Recipe> = ({ item }) => (
+        <View key={item.id} style={{ margin: 5 }}>
+            <Image source={{ uri: item.image }} style={styles.imgStyle} />
+            <Text style={{ color: 'red' }}>{item.title}</Text>
+            <TouchableOpacity onPress={() => saveToShoppingList(item)} style={styles.saveBtnStyle}>
+                <Text>Save Recipe</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+    return (
+        <View style={styles.mainContainer}>
+            <TextInput
+                style={styles.textInputStyle}
+                value={query}
+                onChangeText={setQuery}
+                placeholder="What do you want to eat today?"
+                placeholderTextColor={'#000000'}
+            />
+            <View style={{ padding: 5 }}>
+                <TouchableOpacity
+                    onPress={() => fetchRecipes(query)}
+                    style={styles.searchBtnStyle}
+                >
+                    <Text style={styles.btnText}>Search</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={isListening ? stopListening : startListening}
+                    style={styles.voiceBtnStyle}
+                >
+                    <Text style={styles.btnText}>{isListening ? 'Stop Voice Search' : 'Start Voice Search'}</Text>
+                </TouchableOpacity>
+            </View>
+
+            {error ? <Text>{error}</Text> : null}
+            <View style={styles.flatViewStyle}>
+                <FlatList
+                    data={recipes}
+                    renderItem={renderRecipiesItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+
+            </View>
+
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    mainContainer:{ backgroundColor: '#000000', flex: 1 },
+    textInputStyle :{ margin: 10, borderRadius: 8, padding: 10, backgroundColor: '#ffffff', color: '#000000' },
+    searchBtnStyle:{ padding: 10, backgroundColor: 'red', alignItems: 'center', borderRadius: 8, margin: 10 },
+    voiceBtnStyle:{ padding: 10, backgroundColor: 'blue', alignItems: 'center', borderRadius: 8, margin: 10 },
+    btnText:{ fontWeight: '700' },
+    flatViewStyle:{ backgroundColor: '#ffffff' },
+    imgStyle:{ height: 100, width: 100 },
+    saveBtnStyle:{ backgroundColor: 'blue', alignItems: 'center', padding: 10, marginTop: 5 }
+  });
 export default ChatbotScreen;
